ecb9fe0e030d8ec3586e24ed181fdb40833443f4
Florian Knop <florian_knop@hotmail.com>
2019-03-23 16:18:13 +0100

    start refactor common cp/mip tests

--- code/village1/src/test/scala/village1/modeling/CommonSpec.scala
+++ code/village1/src/test/scala/village1/modeling/CommonSpec.scala
@@ -1,6 +1,8 @@
 package village1.modeling
 
 import org.scalatest.{FunSpec, Matchers}
+import village1.json.JsonParser
+import village1.search.Search
 
 abstract class CommonSpec extends FunSpec with Matchers {
   protected def checkValid (solution: Solution): Unit = {
@@ -14,4 +16,197 @@ abstract class CommonSpec extends FunSpec with Matchers {
   protected def checkPartial(solution: Solution): Unit = {
     solution.partial should equal(true)
   }
-}
+
+  protected def parse(name: String): Problem = {
+    JsonParser.parse(s"data/test/$name")
+  }
+
+
+  protected val checkSolution: Map[String, Search => Unit] = Map(
+    "additional-skills.json" -> additionalSkills_1,
+    "additional-skills-value.json" -> additionalSkills_2,
+    "additional-skills-value-2.json" -> additionalSkills_3,
+    "Iww.json" -> iww_1,
+    "Iwc.json" -> iwc_1,
+    "machines-assignment.json" -> machine_1,
+    "locations-assignment.json" -> location_1,
+  "locations-assignment2.json" -> location_2
+  )
+
+  private def machine_1(search: Search): Unit = {
+    search.onSolutionFound { solution =>
+      checkValid(solution)
+      checkNotPartial(solution)
+
+      val plannings = solution.plannings
+      plannings should have size 2
+
+      val p0 = plannings(0)
+      val p1 = plannings(1)
+
+      p0.machineAssignments.get should contain (0)
+      p0.machineAssignments.get should contain oneOf (1, 2)
+
+      p1.machineAssignments.get should contain oneOf (1, 2)
+
+      if (p0.machineAssignments.get.contains(1)) {
+        p1.machineAssignments.get should contain (2)
+      }
+      else {
+        p0.machineAssignments.get should contain (2)
+        p1.machineAssignments.get should contain (1)
+      }
+    }
+  }
+
+  private def location_1(search: Search): Unit = {
+    search.onSolutionFound { solution =>
+      checkValid(solution)
+      checkNotPartial(solution)
+
+
+      val plannings = solution.plannings
+      plannings should have size 2
+
+      val p0 = plannings(0)
+      val p1 = plannings(1)
+
+      p0.locationAssignment.get should equal(0)
+      p1.locationAssignment.get should equal(0)
+    }
+  }
+
+  private def location_2(search: Search): Unit = {
+    search.onSolutionFound { solution =>
+      checkValid(solution)
+      checkNotPartial(solution)
+
+      val plannings = solution.plannings
+      plannings should have size 2
+
+      val p0 = plannings(0)
+      val p1 = plannings(1)
+
+      val l0 = p0.locationAssignment.get
+      val l1 = p1.locationAssignment.get
+
+      if (l0 == 0) {
+        l0 should equal(0)
+        l1 should equal(1)
+      }
+      else {
+        l0 should equal(1)
+        l1 should equal(0)
+      }
+    }
+  }
+
+
+  private def additionalSkills_1(search: Search): Unit = {
+    search.onSolutionFound { solution =>
+      checkValid(solution)
+      checkNotPartial(solution)
+      val plannings = solution.plannings
+      plannings should have size 2
+
+      val p1 = plannings(1)
+      val workers = p1.workerAssignments.head.workers
+      workers should have size 1
+      workers(0) should not equal 0
+
+      val p0 = plannings(0)
+      val workers0 = p0.workerAssignments.head.workers
+      workers0 should have size 2
+      List(workers0(0), workers0(1)) should contain (0)
+    }
+  }
+
+  private def additionalSkills_2(search: Search): Unit = {
+    search.onSolutionFound { solution =>
+      checkValid(solution)
+      checkNotPartial(solution)
+
+      val plannings = solution.plannings
+      val p = plannings(0)
+
+      val workers = p.workerAssignments.head.workers
+      workers should have size 3
+    }
+  }
+
+  private def additionalSkills_3(search: Search): Unit = {
+    search.onSolutionFound { solution =>
+      checkValid(solution)
+      checkNotPartial(solution)
+
+      val plannings = solution.plannings
+      val p = plannings(0)
+
+      val workers = p.workerAssignments.head.workers
+      workers should have size 3
+    }
+  }
+
+  private def iww_1(search: Search): Unit = {
+    search.onSolutionFound { solution =>
+      checkValid(solution)
+      checkNotPartial(solution)
+
+      val plannings = solution.plannings
+      plannings should have size 2
+
+      val p0 = plannings(0)
+      val p1 = plannings(1)
+
+      val w1 = p0.workerAssignments.head.workers
+      val w2 = p1.workerAssignments.head.workers
+
+      if (w1.contains(0)) {
+        w1 should not contain 1
+        w1 should contain oneOf (2, 3)
+        w2 should contain(1)
+      }
+
+      if (w1.contains(1)) {
+        w1 should not contain 0
+        w1 should contain oneOf (2, 3)
+        w2 should contain(0)
+      }
+
+      if (w1.contains(2)) {
+        w1 should not contain 3
+        w1 should contain oneOf(0, 1)
+        w2 should contain(3)
+      }
+
+      if (w1.contains(3)) {
+        w1 should not contain 2
+        w1 should contain oneOf(0, 1)
+        w2 should contain(2)
+      }
+    }
+  }
+
+
+  private def iwc_1(search: Search): Unit = {
+    search.onSolutionFound { solution =>
+      checkValid(solution)
+      checkNotPartial(solution)
+
+      val plannings = solution.plannings
+      plannings should have size 2
+
+      val p0 = plannings(0)
+      val p1 = plannings(1)
+
+      val w1 = p0.workerAssignments.head.workers
+      val w2 = p1.workerAssignments.head.workers
+
+      w1 should contain only (1, 2)
+      w2 should contain only (0, 3)
+    }
+  }
+
+
+
+  }
--- code/village1/src/test/scala/village1/modeling/mip/VillageOneMIPModelSpec.scala
+++ code/village1/src/test/scala/village1/modeling/mip/VillageOneMIPModelSpec.scala
@@ -1,8 +1,6 @@
 package village1.modeling.mip
 
-import gurobi.GRBException
-import org.scalatest._
-import village1.json.{JsonParser, JsonSerializer}
+import village1.json.JsonParser
 import village1.modeling._
 import village1.search.mip.{MIPSearch, MipSolverResult}
 
@@ -20,59 +18,23 @@ class VillageOneMIPModelSpec extends CommonSpec {
 
   describe("Additional skills for demands") {
     it("Should return the correct workers assigned with additional skills") {
-      val search = getSearch(JsonParser.parse("data/test/additional-skills.json"))
-
-      search.onSolutionFound { solution =>
-        checkValid(solution)
-        checkNotPartial(solution)
-        val plannings = solution.plannings
-        plannings should have size 2
-
-        val p1 = plannings(1)
-        val workers = p1.workerAssignments.head.workers
-        workers should have size 1
-        workers(0) should not equal 0
-
-        val p0 = plannings(0)
-        val workers0 = p0.workerAssignments.head.workers
-        workers0 should have size 2
-        List(workers0(0), workers0(1)) should contain (0)
-      }
-
+      val name = "additional-skills.json"
+      val search = getSearch(parse(name))
+      checkSolution(name)(search)
       solve(search)
     }
 
     it("Should return the correct workers assigned with additional skills (with special Max value)") {
-      val search = getSearch(JsonParser.parse("data/test/additional-skills-value.json"))
-
-      search.onSolutionFound { solution =>
-        checkValid(solution)
-        checkNotPartial(solution)
-
-        val plannings = solution.plannings
-        val p = plannings(0)
-
-        val workers = p.workerAssignments.head.workers
-        workers should have size 3
-      }
-
+      val name = "additional-skills-value.json"
+      val search = getSearch(parse(name))
+      checkSolution(name)(search)
       solve(search)
     }
 
     it("Should return the correct workers assigned with additional skills (with special Min value)") {
-      val search = getSearch(JsonParser.parse("data/test/additional-skills-value-2.json"))
-
-      search.onSolutionFound { solution =>
-        checkValid(solution)
-        checkNotPartial(solution)
-
-        val plannings = solution.plannings
-        val p = plannings(0)
-
-        val workers = p.workerAssignments.head.workers
-        workers should have size 3
-      }
-
+      val name = "additional-skills-value-2.json"
+      val search = getSearch(parse(name))
+      checkSolution(name)(search)
       solve(search)
     }
 
@@ -80,9 +42,6 @@ class VillageOneMIPModelSpec extends CommonSpec {
     it("Should be a partial solution") {
       val search = getSearch(JsonParser.parse("data/test/additional-skills-impossible.json"))
       search.onSolutionFound { solution =>
-        println(solution.plannings.head.workerAssignments(0).workers.mkString(" "))
-        println(solution.plannings.last.workerAssignments(0).workers.mkString(" "))
-
         checkPartial(solution)
       }
 
@@ -116,84 +75,25 @@ class VillageOneMIPModelSpec extends CommonSpec {
 
   describe("Machine assignments") {
     it("Should return the correct machine assignments") {
-      val search = getSearch(JsonParser.parse("data/test/machines-assignment.json"))
-
-      search.onSolutionFound { solution =>
-        checkValid(solution)
-        checkNotPartial(solution)
-
-        val plannings = solution.plannings
-        plannings should have size 2
-
-        val p0 = plannings(0)
-        val p1 = plannings(1)
-
-        p0.machineAssignments.get should contain (0)
-        p0.machineAssignments.get should contain oneOf (1, 2)
-
-        p1.machineAssignments.get should contain oneOf (1, 2)
-
-        if (p0.machineAssignments.get.contains(1)) {
-          p1.machineAssignments.get should contain (2)
-        }
-        else {
-          p0.machineAssignments.get should contain (2)
-          p1.machineAssignments.get should contain (1)
-        }
-      }
-
+      val name = "machines-assignment.json"
+      val search = getSearch(parse(name))
+      checkSolution(name)(search)
       solve(search)
     }
   }
 
   describe("Location assignments") {
     it("Should have the same location assigned") {
-      val search = getSearch(JsonParser.parse("data/test/locations-assignment.json"))
-
-      search.onSolutionFound { solution =>
-        checkValid(solution)
-        checkNotPartial(solution)
-
-
-        val plannings = solution.plannings
-        plannings should have size 2
-
-        val p0 = plannings(0)
-        val p1 = plannings(1)
-
-        p0.locationAssignment.get should equal(0)
-        p1.locationAssignment.get should equal(0)
-      }
-
+      val name = "locations-assignment.json"
+      val search = getSearch(parse(name))
+      checkSolution(name)(search)
       solve(search)
     }
 
     it("Should have different location assigned") {
-      val search = getSearch(JsonParser.parse("data/test/locations-assignment2.json"))
-
-      search.onSolutionFound { solution =>
-        checkValid(solution)
-        checkNotPartial(solution)
-
-        val plannings = solution.plannings
-        plannings should have size 2
-
-        val p0 = plannings(0)
-        val p1 = plannings(1)
-
-        val l0 = p0.locationAssignment.get
-        val l1 = p1.locationAssignment.get
-
-        if (l0 == 0) {
-          l0 should equal(0)
-          l1 should equal(1)
-        }
-        else {
-          l0 should equal(1)
-          l1 should equal(0)
-        }
-      }
-
+      val name = "locations-assignment2.json"
+      val search = getSearch(parse(name))
+      checkSolution(name)(search)
       solve(search)
     }
   }
@@ -202,46 +102,9 @@ class VillageOneMIPModelSpec extends CommonSpec {
   describe("Worker/worker incompatibilities") {
 
     it("Should take worker/worker incompatibilities into account") {
-      val search = getSearch(JsonParser.parse("data/test/Iww.json"))
-
-      search.onSolutionFound { solution =>
-        checkValid(solution)
-        checkNotPartial(solution)
-
-        val plannings = solution.plannings
-        plannings should have size 2
-
-        val p0 = plannings(0)
-        val p1 = plannings(1)
-
-        val w1 = p0.workerAssignments.head.workers
-        val w2 = p1.workerAssignments.head.workers
-
-        if (w1.contains(0)) {
-          w1 should not contain 1
-          w1 should contain oneOf (2, 3)
-          w2 should contain(1)
-        }
-
-        if (w1.contains(1)) {
-          w1 should not contain 0
-          w1 should contain oneOf (2, 3)
-          w2 should contain(0)
-        }
-
-        if (w1.contains(2)) {
-          w1 should not contain 3
-          w1 should contain oneOf(0, 1)
-          w2 should contain(3)
-        }
-
-        if (w1.contains(3)) {
-          w1 should not contain 2
-          w1 should contain oneOf(0, 1)
-          w2 should contain(2)
-        }
-      }
-
+      val name = "Iww.json"
+      val search = getSearch(parse(name))
+      checkSolution(name)(search)
       solve(search)
     }
   }
@@ -249,25 +112,9 @@ class VillageOneMIPModelSpec extends CommonSpec {
   describe("Worker/client incompatibilities") {
 
     it("Should take worker/client incompatibilities into account") {
-      val search = getSearch(JsonParser.parse("data/test/Iwc.json"))
-
-      search.onSolutionFound { solution =>
-        checkValid(solution)
-        checkNotPartial(solution)
-
-        val plannings = solution.plannings
-        plannings should have size 2
-
-        val p0 = plannings(0)
-        val p1 = plannings(1)
-
-        val w1 = p0.workerAssignments.head.workers
-        val w2 = p1.workerAssignments.head.workers
-
-        w1 should contain only (1, 2)
-        w2 should contain only (0, 3)
-      }
-
+      val name ="Iwc.json"
+      val search = getSearch(parse(name))
+      checkSolution(name)(search)
       solve(search)
     }
   }
@@ -281,7 +128,7 @@ class VillageOneMIPModelSpec extends CommonSpec {
         checkNotPartial(solution)
       }
 
-      search.solve()
+      solve(search)
     }
   }
 
