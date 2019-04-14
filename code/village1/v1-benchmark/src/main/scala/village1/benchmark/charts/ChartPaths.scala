b4b1b733ec84c2561bde4976edf4f8ceec07f394
Florian Knop <florian_knop@hotmail.com>
2019-04-14 11:49:45 +0200

    add line-chart

--- code/village1/v1-benchmark/src/main/scala/village1/benchmark/charts/ChartPaths.scala
+++ code/village1/v1-benchmark/src/main/scala/village1/benchmark/charts/ChartPaths.scala
@@ -4,6 +4,6 @@ object ChartPaths {
   val Template: String = "templates"
   val PerformanceProfile: String = s"$Template/performance-profile.html"
   val OOT: String = s"$Template/objective-over-time.html"
-  val Line: String = s"$Template/column-chart.html"
-  val Column: String = s"$Template/line-chart.html"
+  val Line: String = s"$Template/line-chart.html"
+  val Column: String = s"$Template/column-chart.html"
 }
